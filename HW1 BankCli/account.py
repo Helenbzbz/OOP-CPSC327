539ecb2c729dd459331fa9197a9623a76af07fcb
Helen Zheng <jzheng3@babson.edu>
2024-02-01 13:49:21 -0500

    ok

---
+++ HW1 BankCli/account.py
@@ -0,0 +1,122 @@
+from datetime import timedelta
+import datetime
+from decimal import Decimal
+
+class Account():
+    """
+    Represent an Account in the Bank. This is the parent class for  Checking and Savings accounts.
+    Stores list of transactions, balance, and latest transaction date.
+    Includes methods to: 
+    1. add transaction to lists of transactions
+    2. List all transactions
+    3. Check if a new transaction will violate the balance limit
+    4. Interest and fee: Add a transaction to the list of transactions with the interest: total_balance * monthly interest rate; Add fee if balance is less than 100
+    """
+
+    # Store the next available id for all new notes
+    last_id = 0
+
+    def __init__(self, balance = Decimal(0), transactions = [], latest_transaction_date = None):
+        """initialize a note with memo and optional
+        space-separated tags. Automatically set the note's
+        creation date and a unique id."""
+
+        self.balance = balance
+        self._transactions = transactions
+        self._latest_transaction_date = latest_transaction_date
+
+        Account.last_id += 1
+        self.id = Account.last_id
+
+    def transaction_record(self, amount, date):
+        """Add a transaction with the amount and date to the list of transactions, sorted by date"""
+        self._transactions.append((amount, date))
+        self._transactions.sort(key=lambda x: x[1])
+
+        if not self._latest_transaction_date:
+            self._latest_transaction_date = date
+        else:
+            self._latest_transaction_date = max(self._latest_transaction_date, date)
+
+        self.balance += amount
+
+    def list_transactions(self):
+        """List all transactions"""
+        for transaction in self._transactions:
+            print(f'{transaction[1]}, ${transaction[0]:,.2f}')
+
+    def check_balance_limit(self, amount):
+        """Check if a new transaction will violate the balance limit"""
+        if self.balance + amount < 0:
+            return False
+        return True
+
+    def interest_and_fee_base(self, interest_rate):
+        """Add a transaction to the list of transactions with the interest: total_balance * monthly interest rate
+        Add a fee: $5.44 if balance is less than 100"""
+
+        ## Deal with inetrest
+        interest = self.balance * interest_rate
+
+        ## Find the last day of the month of latest transaction
+        next_month = self._latest_transaction_date.replace(day=28) + timedelta(days=4) 
+        latest_month_end = next_month - timedelta(days=next_month.day)
+
+        ## Adjsut the interest to the last day of the month of latest transaction
+        self.transaction_record(interest, latest_month_end)
+        self.balance += interest
+
+        ## Deal with fee
+        if self.balance < 100:
+            self.balance -= 5.44
+            self.transaction_record(-5.44, latest_month_end)
+
+class CheckingAccount(Account):
+    """This is a Checking Account. It is a child class of Account.
+    It has 2 methods defined further than parent class:
+    1. add_transaction: Add a transaction to the list of transactions if it does not violate the balance limit
+    2. interest_and_fee: with interest rate of 0.08% and fee of $5.44"""
+    def __init__(self):
+        super().__init__()
+        self.type = 'Checking'
+
+    def interest_and_fee(self):
+        """Call the interest_and_fee_base method with interest rate of 0.08% and fee of $5.44"""
+        self.interest_and_fee_base(Decimal(0.0008))
+
+    def add_transaction(self, date, amount):
+        """Add a transaction to the list of transactions if it does not violate the balance limit"""
+        if self.check_balance_limit(amount):
+            self.transaction_record(amount, date)
+
+class SavingsAccount(Account):
+    """This is a Savings Account. It is a child class of Account.
+    It has 2 methods defined further than parent class:
+    1. add_transaction: Add a transaction to the list of transactions if it does not violate the balance limit and if it does not violate the frequency limit
+    2. interest_and_fee: with interest rate of 0.41% and fee of $5.44"""
+
+    def __init__(self):
+        super().__init__()
+        self.type = 'Savings'
+
+    def interest_and_fee(self):
+        """Call the interest_and_fee_base method with interest rate of 0.41% and fee of $5.44"""
+        self.interest_and_fee_base(Decimal(0.0041))
+
+    def check_frequency_limit(self, date):
+        """Check if a new transaction will violate the frequency limit, daily limit = 2, monthly limit = 5"""
+        day_count, month_count = 0, 0
+        for transaction in self._transactions:
+            if transaction[1] == date:
+                day_count += 1
+            if transaction[1].month == date.month:
+                month_count += 1
+            if day_count >= 2 or month_count >= 5:
+                return False
+        return True
+
+    def add_transaction(self, date, amount):
+        """Add a transaction to the list of transactions if it does not violate the balance limit and if it does not violate the frequency limit"""
+        if self.check_balance_limit(amount) and self.check_frequency_limit(date):
+            self.transaction_record(amount, date)
+
---
+++ HW1 BankCli/cli.py
@@ -0,0 +1,89 @@
+import sys
+import pickle
+from bank import Bank
+from datetime import datetime
+from decimal import Decimal
+
+class BankCli():
+    """Display a menu and respond to choices when run."""
+
+    def __init__(self):
+        self._bank = Bank()
+        self.current_account = None
+        self._choices = {
+            "1": self._open_account,
+            "2": self._summary,
+            "3": self._select_account,
+            "4": self._add_transaction,
+            "5": self._list_transactions,
+            "6": self._interest_and_fee,
+            "7": self._save,
+            "8": self._load,
+            "9": self._quit
+        }
+
+    def _display_menu(self):
+        if self.current_account == None:
+            text_for_account = 'None'
+        else:
+            text_for_account = f'{self.current_account.type}#{str(self.current_account.id).zfill(9)}, balance: ${self.current_account.balance:,.2f}'
+        print("--------------------------------")
+        print(f"Currently selected account: {text_for_account}")
+        print("Enter command\n"
+        "1: open account\n"
+        "2: summary\n"
+        "3: select account\n"
+        "4: add transaction\n"
+        "5: list transactions\n"
+        "6: interest and fees\n"
+        "7: save\n"
+        "8: load\n"
+        "9: quit")
+
+    def run(self):
+        """Display the menu and respond to choices."""
+        while True:
+            self._display_menu()
+            choice = input("Enter an option: ")
+            action = self._choices.get(choice)
+            if action:
+                action()
+            else:
+                print("{0} is not a valid choice".format(choice))
+
+    def _open_account(self):
+        type = input("Type of account? (checking/savings)\n")
+        self._bank.open_account(type)
+
+    def _summary(self):
+        self._bank.summary()
+
+    def _select_account(self):
+        account_id = input("Enter account number\n")
+        self.current_account = self._bank.select_account(int(account_id))
+
+    def _add_transaction(self):
+        amount = Decimal(input("Amount?\n"))
+        date_input = input("Date? (YYYY-MM-DD)\n")
+        date = datetime.strptime(date_input, "%Y-%m-%d").date()
+        self.current_account.transaction_record(amount, date)
+
+    def _list_transactions(self):
+        self.current_account.list_transactions()
+
+    def _interest_and_fee(self):
+        self.current_account.interest_and_fee()
+
+    def _save(self):
+        with open("bank_save.pickle", "wb") as f:
+            pickle.dump(self._bank, f)
+
+    def _load(self):
+        with open("bank_save.pickle", "rb") as f:   
+            self._bank = pickle.load(f)
+
+    def _quit(self):
+        sys.exit(0)
+
+if __name__ == "__main__":
+    BankCli().run()
