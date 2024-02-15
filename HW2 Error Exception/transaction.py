43e2b0cbd22890a6cad11132884f296c6af60779
Helen Zheng <jzheng3@babson.edu>
2024-02-15 10:39:49 -0500

    Update transaction.py

--- HW2 Error Exception/transaction.py
+++ HW2 Error Exception/transaction.py
@@ -1,7 +1,7 @@
 class Transaction():
     """This is a Transaction class that stores: date, amount, type (1: user_initiated; 0: system_initiated/fees & interests)"""
     def __init__(self, amount, type, date):
-        self._amount = amount
+        self.amount = amount
         self.type = type
         self.date = date
     
